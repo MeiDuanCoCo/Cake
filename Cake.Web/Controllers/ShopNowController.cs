f6b568861a259839b4ffd8f2d82f70947930b524
美短CoCo <Administrator@WIN-0200CASJGNB>
2017-07-18 11:11:45 +0800

    add projects

---
+++ Cake.Web/Controllers/ShopNowController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cake.Web.Controllers
+{
+    public class ShopNowController : Controller
+    {
+        // GET: ShopNow
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShopStore()
+        {
+            return View();
+        }
+    }
+}
